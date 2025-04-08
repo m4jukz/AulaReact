17f4c6e994b7e9e67d50aaae0d7bca3df6f88c4d
m4jukz <majuchagas08@gmail.com>
2025-04-08 14:57:51 -0300

    08/04
    maju

---
+++ boasvindas/src/App.jsx
@@ -0,0 +1,21 @@
+import BoasVindas from "./Exemplo1"
+function App() {
+  
+
+  return (
+ <div>
+ <h1>Boas Vindas ao React com JavaScript</h1> 
+ <p>Este é o arquivo App.jsx que será o nosso ponto de partida para o desenvolvimento de aplicação React ultilizando JavaScript.
+
+ </p>
+ <BoasVindas nome="Maju"/>
+ <BoasVindas nome="Camila"/>
+ <BoasVindas nome="Leticia"/>
+ <BoasVindas nome="Rafael"/>
+ <BoasVindas nome="Jão"/>
+
+ </div>  
+  )
+}
+
+export default App
