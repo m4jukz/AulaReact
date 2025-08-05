9151e257a4a9a02ba90c124e6c56a43a04f44efb
m4jukz <majuchagas08@gmail.com>
2025-08-05 14:51:28 -0300

    05/08

---
+++ estudorotas/src/Paginas/Home.jsx
@@ -0,0 +1,13 @@
+export default function Home ()
+{
+    return(
+        <div>
+            <h1>Estudo de Rotas no React</h1>
+<ul>
+    <li><a href="/sp">São Paulo</a></li>
+</ul>
+
+        </div>
+    )
+}
+    
---
+++ estudorotas/src/Paginas/SaoPaulo.jsx
@@ -0,0 +1,23 @@
+export default function SaoPaulo()
+{
+    return(
+        <div>
+            <h1>Seja bem vindo ao Estado de São Paulo</h1>
+            <p>
+                São Paulo é uma cidade gigante e cheia de vida.
+                Aqui se mistura gente de todo lugar e muita cultura.
+                Tem prédios altos, parques e muita movimentação.
+                É o coração econômico e cultural do Brasil.
+
+            </p>
+            <p>
+                <img src=""/>
+            </p>
+            
+            <p>
+                <a href="/">Voltar para Home</a>
+            </p>
+        </div>
+
+    )
+}
