6171b6f514f5dbd6d54b346733c001cd8f4521f3
m4jukz <majuchagas08@gmail.com>
2025-09-23 14:52:19 -0300

    23/09

--- estudoestados/src/Paginas/Exemplo1.jsx
+++ estudoestados/src/Paginas/Exemplo1.jsx
@@ -1,28 +1,67 @@
+import { useState } from "react";
+
 export default function Exemplo1() {
+
+  function somar() {
+
+    let n1, n2, soma;
+    n1 = Number(numero1);
+    n2 = Number(numero2);
+    soma = n1 + n2; 
+   
+    setResultado (" A soma dos números é " + soma);
+
+  }
+  function limpar() {
+
+  }
+
+  const [numero1, setNumero1] = useState(22);
+  const [numero2, setNumero2] = useState(5);
+  const [resultado, setResultado] = useState();
+
+
   return (
     <div>
       <h1>Exemplo 1</h1>
-<div className="conteudo">
-  <h3>Exemplo do UseState</h3>
-  <p>
-    O objetivo aqui sera receber dois números, soma-los, e exibir o resultado. 
-  </p>
-  <form>
-    <p>
-      Digite o primeiro número<br/>
-      <input type="text"/>
-    </p>
-    <p>
-      Digite o segundo número<br/>
-      <input type="text"/>
-    </p>
-    <p>
-      <input type="button" value="Calcular"/>
-    </p>
-  </form>
-  <a href="/">Voltar</a>
-</div>
-      
+
+      <div className="conteudo">
+
+        <h3>Exemplo do UseState</h3>
+
+        <p>
+          O objetivo aqui sera receber dois números, soma-los, e exibir o resultado.
+        </p>
+
+        <form>
+          <p>
+            Digite o primeiro número<br />
+            <input type="text" value={numero1}
+              onChange={(e) => setNumero1(e.target.value)} />
+          </p>
+
+          <p>
+            Digite o segundo número<br />
+            <input type="text" value={numero2}
+              onChange={(e) => setNumero2(e.target.value)} />
+
+          </p>
+
+          <p>
+            <input type="button" value="Calcular"
+              onClick={somar} />
+          </p>
+
+          <p>
+            Número1 = {numero1} <br />
+            Número2 = {numero2}
+            {resultado}
+          </p>
+
+        </form>
+        <a href="/">Voltar</a>
+      </div>
+
     </div>
   );
 }
--- estudoestados/src/Paginas/Exemplo2.jsx
+++ estudoestados/src/Paginas/Exemplo2.jsx
@@ -1,4 +1,24 @@
+import { useState } from "react";
+
 export default function Exemplo2() {
+  function media() {
+
+    let n1, n2, media;
+    n1 = Number(numero1);
+    n2 = Number(numero2);
+    media = n1 + n2 /2; 
+   
+    setResultado (" A média dos números é " + media);
+  }
+  function limpar() {
+
+  }
+
+  const [numero1, setNumero1] = useState(22);
+  const [numero2, setNumero2] = useState(5);
+  const [resultado, setResultado] = useState();
+
+
   return (
     <div>
       <h1>Exemplo 2</h1>
@@ -9,22 +29,31 @@ export default function Exemplo2() {
   </p>
         <p>
         Nome do Aluno:<br/>
-      <input type="text"/>  
+      <input type="text" /> 
         </p>
 
         <p>
         Nota 1:<br/>
-      <input type="text"/>  
+      <input type="text" value={numero1}
+              onChange={(e) => setNumero1(e.target.value)} />
         </p>
 
         <p>
         Nota 2:<br/>
-      <input type="text"/>  
+      <input type="text"value={numero2}
+              onChange={(e) => setNumero2(e.target.value)} /> 
         </p>
 
         <p>
-      <input type="button" value="Calcular"/>
+      <input type="button" value="Calcular"
+      onClick={media} />
     </p>
+    <p>
+            Número1 = {numero1} <br />
+            Número2 = {numero2}
+            {resultado}
+          </p>
+
     <a href="/">Voltar</a>
       </div>
 
